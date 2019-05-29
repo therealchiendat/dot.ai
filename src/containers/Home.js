1b35bd31a3b6ab078768f7a0d285ac94497aaf6e
therealchiendat <chiendat@gmail.com>
2019-05-29 17:56:29 -0300

    Initialize React App

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>dot.ai</h1>
+          <p>Comma.ai challenge end with a dot :P</p>
+        </div>
+      </div>
+    );
+  }
+}
