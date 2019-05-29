71b2fc63bb9ecb605f39344b5b01def80618c875
therealchiendat <chiendat@gmail.com>
2019-05-29 19:47:59 -0300

    add Map

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,55 @@
 import React, { Component } from "react";
+import {Map, InfoWindow, Marker, GoogleApiWrapper,Polyline} from 'google-maps-react';
 import "./Home.css";
 
-export default class Home extends Component {
+export class Home extends React.Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
   render() {
+    const style = {
+      width: 'auto',
+      height: '100vh',
+      position: 'relative'
+    }
     return (
-      <div className="Home">
-        <div className="lander">
-          <h1>dot.ai</h1>
-          <p>Comma.ai challenge end with a dot :P</p>
-        </div>
+
+      <div container style={style}>
+      <Map google={this.props.google}
+          onClick={this.onMapClicked}>
+        <Marker onClick={this.onMarkerClick}
+                name={'Location tag'} />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
       </div>
-    );
+    )
   }
 }
+ export default GoogleApiWrapper({
+   apiKey: 'AIzaSyDu83zIs6jrdnI8xI_GtnhFvFWAEbdxZ6E'
+ })(Home)
